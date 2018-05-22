36fb7a394920a0a73f82d04dfd0365461053fe46
Malyasse <murphy04@hotmail.fr>
2018-05-22 15:05:43 +0200

    Creation fichier labyrinthe

---
+++ labyrinthe.h
@@ -0,0 +1,14 @@
+#ifndef LABYRINTHE_H_INCLUDED
+#define LABYRINTHE_H_INCLUDED
+
+#include "point.h";
+
+typedef struct {
+  point* pos_bg; //position bas "gauche" de la piece
+  
+} piece;
+
+
+
+
+#endif
