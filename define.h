63ee3c8e479d964a7b4228f32dec62215fb664de
Malyasse <murphy04@hotmail.fr>
2018-05-24 06:10:33 +0200

    define mise a jour

--- define.h
+++ define.h
@@ -5,11 +5,17 @@
 #define AXE_Y 1
 #define AXE_Z 2
 #define TAILLE 5
-#define TAILLE_PIECE 10
-#define HAUTEUR 3
-#define LARGEUR 3
-#define PROFONDEUR 3
+#define TAILLE_PIECE 15
+#define HAUTEUR 10
+#define LARGEUR 12
+#define PROFONDEUR 10
 #define NOMBRE_PIECE 20
 #define VUE 10
-#define NON_VUE 0
+#define NON_VUE -1
+#define NON_TRAITEE 5
+#define PIECE_DEPART 15
 #endif
+#include "GL/gl.h"
+#include "GL/glut.h"
+
+GLuint texture1;
