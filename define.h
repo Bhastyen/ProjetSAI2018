c264730ced61ac75bd7153c24885469e47324911
Malyasse <murphy04@hotmail.fr>
2018-05-24 11:48:25 +0200

    ajout

--- define.h
+++ define.h
@@ -5,17 +5,16 @@
 #define AXE_Y 1
 #define AXE_Z 2
 #define TAILLE 5
-#define TAILLE_PIECE 15
+#define TAILLE_PIECE 25
 #define HAUTEUR 10
-#define LARGEUR 12
+#define LARGEUR 10
 #define PROFONDEUR 10
-#define NOMBRE_PIECE 20
+#define NOMBRE_PIECE 6
 #define VUE 10
 #define NON_VUE -1
 #define NON_TRAITEE 5
-#define PIECE_DEPART 15
+#define PIECE_DEPART 3
+#define VIDE -1
 #endif
-#include "GL/gl.h"
-#include "GL/glut.h"
 
-GLuint texture1;
+
